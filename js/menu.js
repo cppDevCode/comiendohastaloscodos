66d11f33e520952a9ab7513f52ee84d762a8ab31
alejandro.baldres@gmail.com <alejandro.baldres@gmail.com>
2024-04-21 19:51:25 -0300

    Primera version Index, sin agregado de carrousel y mapa

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+let btnMenu = document.getElementById('btnMenu');
+let btnHome = document.getElementById('btnHome');
+
+btnMenu.onclick = function(){
+    location.href='#menuResto';
+}
+
+btnHome.onclick = function() {
+    location.href='./index.html'
+}
