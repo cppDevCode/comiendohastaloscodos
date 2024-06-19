a6afb96790be1332ac28f3c08f8850b7311bbc25
alejandro.baldres@gmail.com <alejandro.baldres@gmail.com>
2024-06-19 18:04:46 -0300

    organizacion del codigo back

--- backEnd/main.py
+++
@@ -1,6 +0,0 @@
-import repository.repositoryDB as db
-
-basedatos = db.baseDeDatos("prueba","XXXX")
-basedatos.creoBaseDeDatos("comiendohastaloscodos")
-basedatos.creoTablas("comiendohastaloscodos")
-basedatos.cierroConeccion()
